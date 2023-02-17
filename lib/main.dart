dea9542c0015f768d190cd3858d8de5967ac62d7
Ziad Elsdaadany <ziadelsaadany73@gmail.com>
2023-02-17 18:29:06 +0200

    add observer

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_online_adding_bloc/cubit/news_cubit.dart';
 import 'package:news_online_adding_bloc/home_view.dart';
 
+import 'bloc_observer.dart';
+
 void main(  ) {
+  Bloc.observer = MyBlocObserver();
   runApp(const NewsAppWithBloc() );
 }
 class NewsAppWithBloc extends StatelessWidget {
